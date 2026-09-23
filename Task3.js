50de0894973bf3ac307e0dfe82251c08286eaade
Deyaa Alwardat <deyaaaalwardat@gmail.com>
2026-09-23 21:16:15 +0300

    last update Task 3

--- Task3.js
+++ Task3.js
@@ -17,7 +17,7 @@ function displayOrder() {
 
 //changeButtonColor
 function changeButtonColor() {
- document.getElementById("submitBtn").style.backgroundColor="green";
+ document.getElementById("submitBtn").style.backgroundColor= "green";
 }
 
 //resetButtonColor
@@ -30,19 +30,15 @@ function resetButtonColor() {
 let textBox = document.getElementById("text-box");
 
 // 2. getElementsByClassName()
-let container =
-    document.getElementsByClassName("controls-container")[0];
+let container = document.getElementsByClassName("controls-container")[0];
 
 // 3. getElementsByTagName()
-let allInputs =
-    document.getElementsByTagName("input");
+let allInputs =document.getElementsByTagName("input");
 
 // 4. querySelector()
-let fontSelect =
-    document.querySelector("#fontFamily");
+let fontSelect =document.querySelector("#fontFamily");
 
-let sizeSelect =
-    document.querySelector("#fontSize");
+let sizeSelect =document.querySelector("#fontSize");
 
 
 
@@ -50,48 +46,27 @@ let sizeSelect =
 function updateTextStyles() {
 
     let font = fontSelect.value;
-
     let size = sizeSelect.value;
-
-    let isItalic =
-        document.getElementById("italic").checked;
-
-    let isBold =
-        document.getElementById("bold").checked;
-
-    let isUnderline =
-        document.getElementById("underline").checked;
-
+    let isItalic = document.getElementById("italic").checked;
+    let isBold =document.getElementById("bold").checked;
+    let isUnderline =document.getElementById("underline").checked;
 
     // Change text style
-
     textBox.style.fontFamily = font;
-
     textBox.style.fontSize = size;
-
-    textBox.style.fontStyle =
-        isItalic ? "italic" : "normal";
-
-    textBox.style.fontWeight =
-        isBold ? "bold" : "normal";
-
-    textBox.style.textDecoration =
-        isUnderline ? "underline" : "none";
+    textBox.style.fontStyle =isItalic ? "italic" : "normal";
+    textBox.style.fontWeight =isBold ? "bold" : "normal";
+    textBox.style.textDecoration =isUnderline ? "underline" : "none";
 }
 
 
 // Do While + onclick
-
 let i = 0;
-
 do {
 
     allInputs[i].onclick = function() {
-
         updateTextStyles();
-
     };
-
     i++;
 
 } while (i < allInputs.length);
@@ -104,18 +79,14 @@ sizeSelect.onchange = updateTextStyles;
 
 // onmouseover
 textBox.onmouseover = function() {
-
     textBox.style.backgroundColor = "#e8f4f8";
-
 };
 
 
 
 // onmouseout
 textBox.onmouseout = function() {
-
     textBox.style.backgroundColor = "white";
-
 };
 
 
@@ -126,75 +97,25 @@ updateTextStyles();
 
 // Image Shape Changer
 // getElementById()
-var img = document.getElementById("target-image");
-
-var btnCircle = document.getElementById("btn-circle");
-
-var btnSquare = document.getElementById("btn-square");
-
-
+let img = document.getElementById("target-image");
+let btnCircle = document.getElementById("btn-circle");
+let btnSquare = document.getElementById("btn-square");
 // querySelector()
-var title = document.querySelector("#main-title");
-
-
+let title = document.querySelector("#main-title");
 // getElementsByClassName()
-var buttonsByClass =
-    document.getElementsByClassName("shape-btn");
-
-
+let buttonsByClass =document.getElementsByClassName("shape-btn");
 // getElementsByTagName()
-var allButtons =
-    document.getElementsByTagName("button");
-
+let allButtons =document.getElementsByTagName("button");
 
 // onclick
-
-
 // Make Circle
 btnCircle.onclick = function() {
 
     img.style.borderRadius = "50%";
 
 };
-
-
 // Make Square
 btnSquare.onclick = function() {
-
     img.style.borderRadius = "0%";
 
 };
-
-
-// do...while
-
-
-if (allButtons.length > 0) {
-
-    let i = 0;
-
-    do {
-
-        let currentBtn = allButtons[i];
-
-
-        // onmouseover
-        currentBtn.onmouseover = function() {
-
-            currentBtn.style.backgroundColor = "#e0e0e0";
-
-        };
-
-
-        // onmouseout
-        currentBtn.onmouseout = function() {
-
-            currentBtn.style.backgroundColor = "#f9f9f9";
-
-        };
-
-
-        i++;
-
-    } while (i < allButtons.length);
-}
